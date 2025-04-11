0140860d86e7e7fd05c9880a494619e5a23303f7
ik3ef3 <spiri2604@gmail.com>
2025-04-11 07:22:59 +0300

    доработал медиа

--- dostavka/dostavka/settings.py
+++ dostavka/dostavka/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'program/static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'program/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- dostavka/program/models.py
+++ dostavka/program/models.py
@@ -26,7 +26,7 @@ class Dish(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     ingredients = models.ManyToManyField(Ingredient, through='DishIngredient')
-    image = models.ImageField(upload_to='program/media/menu_images/', blank=True, null=True)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
 
     def __str__(self):
         return self.name
